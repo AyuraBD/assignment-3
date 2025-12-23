5689a5112147628be4bb774776159f203e4f15e2
Mohammad Ayub <61299782+AyuraBD@users.noreply.github.com>
2025-12-23 16:29:50 +0600

    Change JOINs to INNER JOINs in queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 
 SELECT b.booking_id, u.name AS customer_name, v.name AS vehicle_name, b.start_date, b.end_date, b.status FROM bookings b
-JOIN users u ON b.user_id = u.user_id
-JOIN vehicles v ON b.vehicle_id = v.vehicle_id;
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN vehicles v ON b.vehicle_id = v.vehicle_id;
 
 SELECT v.vehicle_id, v.name AS vehicle_name, v.type, v.model, v.rental_price, v.status from vehicles v
 LEFT JOIN bookings b ON v.vehicle_id = b.booking_id 
